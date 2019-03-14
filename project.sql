3ff52a2863a0f4390f8eccce84a2c0a241a88d47
Kevin Wong <kev.wong97@gmail.com>
2019-03-13 17:17:28 -0700

    added some API endpoints for fetching/adding to the database and sql script
    added project.sql
    in mysql, drop database project and then run "source [path to project.sql]", which will create all the tables from the schema and insert all the rows.
    changes to schema:
    -removed RestaurantContactInfo
    -menuItem is now a proper strong entity, so it has its own MenuItemID
    -customer now has customerUsername instead of customerID as its PK (reason is because a website or service usually has unique usernames for users)
    -restaurant has its own restaurant ID now which should make it easier when we want to save this information on the frontend state
    
    for endpoints:
    all getter endpoints will take arguments
    all post endpoints wil take an object in the request form that matches the schema of whatever we're trying to add
    
    only have simple ones for now like most getters and some adding

---
+++ project.sql
@@ -0,0 +1,209 @@
+create database project;
+use project;
+
+create table Customer (
+  	CustomerUsername char(30),
+  	CustomerPassword char(30),
+	EmailAddress char(30),
+	PhoneNumber char(20),
+	Address char(50),
+	PRIMARY KEY (CustomerUserName)
+);
+
+create table CreditCard (
+	CreditCardNumber char(20),
+	BillingAddress char(100),
+	DateExpire DATE,
+	NameOnCard char(30),
+	CustomerUsername char(30) NOT NULL,
+	PRIMARY KEY (CreditCardNumber),
+	FOREIGN KEY (CustomerUsername) REFERENCES Customer(CustomerUsername)
+);
+
+create table Restaurant (
+	RestaurantID integer,
+	Name char(30),
+	Location char(100),
+	Category char(30),
+	Rating integer,
+	DeliveryFee REAL,
+	PRIMARY KEY (RestaurantID)
+);
+
+create table DeliveryPerson(
+	Name char(30),
+	Address char(100),
+	Tips REAL,
+	Rating REAL,
+	EmailAddress char(50),
+	BankAccountNumber char(20),
+	DateJoined DATE,
+  	PRIMARY KEY (Name, Address)
+);
+
+create table Biker (
+	Name char(30),
+	Address char(100),
+	MaximumDistance integer,
+	BikeDescription char(200),
+	PRIMARY KEY (Name, Address),
+FOREIGN KEY (Name, Address) REFERENCES DeliveryPerson(Name, 	Address)
+);
+
+create table DriverVehicleInformation (
+	LicensePlateNumber char(10),
+	DriverLicenseNumber char(30),
+	VehicleDescription char(200),
+	ValidInsurance bit,
+  	PRIMARY KEY (LicensePlateNumber, DriverLicenseNumber)
+);
+
+create table Driver (
+	Name char(30),
+	Address char(100),
+	LicensePlateNumber char(10),
+	DriverLicenseNumber char(30),
+	PRIMARY KEY (Name, Address),
+FOREIGN KEY (Name, Address) REFERENCES DeliveryPerson(Name, 	Address),
+FOREIGN KEY (LicensePlateNumber, DriverLicenseNumber) 			REFERENCES DriverVehicleInformation(LicensePlateNumber, 		DriverLicenseNumber)
+);
+
+
+create table RestaurantOrder (
+	OrderID integer,
+	Date DATE,
+	Time TIME,
+	Price REAL,
+	Distance integer,
+	TipAmount REAL,
+	Status char(20),
+	Location char(100),
+	CustomerUsername char(30) NOT NULL,
+	CreditCardNumber char(20) NOT NULL,
+	DeliveryPersonName char(30) NOT NULL,
+  	DeliveryPersonAddress char(50) NOT NULL,
+	RestaurantID integer NOT NULL,
+	PRIMARY KEY (OrderID),
+	FOREIGN KEY (CustomerUsername) REFERENCES Customer(CustomerUsername),
+	FOREIGN KEY (CreditCardNumber) REFERENCES 						CreditCard(CreditCardNumber),
+FOREIGN KEY (DeliveryPersonName, DeliveryPersonAddress) 		REFERENCES DeliveryPerson(Name, Address),
+FOREIGN KEY (RestaurantID) REFERENCES 	Restaurant(RestaurantID)
+);
+
+create table Menu (
+	MenuID integer,
+	Name char(30),
+	TimeFrom TIME,
+	TimeTo TIME,
+	RestaurantID integer NOT NULL,
+	PRIMARY KEY (MenuID),
+FOREIGN KEY (RestaurantID) REFERENCES 	Restaurant(RestaurantID)
+);
+
+create table MenuCategory (
+	MenuCategoryID integer,
+	Name char(30),
+	MenuID integer NOT NULL,
+	PRIMARY KEY (MenuCategoryID),
+	FOREIGN KEY (MenuID) REFERENCES Menu(MenuID)
+);
+create table MenuItem (
+  	MenuItemID integer,
+	Name char(30),
+  	MenuCategoryID integer NOT NULL,
+	Price REAL,
+	Calories integer,
+	Description char(100),
+	Rating integer,
+	PRIMARY KEY (MenuItemID),
+	FOREIGN KEY (MenuCategoryID) REFERENCES	MenuCategory(MenuCategoryID) ON DELETE CASCADE
+);
+
+create table OrderedMenuItem (
+	OrderID integer,
+	MenuItemID integer,
+	Quantity integer,
+	SpecialInstructions char(100),
+	PRIMARY KEY (OrderID, MenuItemID),
+	FOREIGN KEY (OrderID) REFERENCES RestaurantOrder(OrderID),
+	FOREIGN KEY (MenuItemID) REFERENCES MenuItem(MenuItemID)
+);
+
+
+INSERT INTO Customer VALUES ("rsitu", "rsitupw", "raymond.situ@hotmail.com", "6045557389", "2329 West Mall, Vancouver, BC V6T 1Z4");
+INSERT INTO Customer VALUES ("kwong", "kwongpw", "kev.wong97@gmail.com", "6045551234", "2330 West Mall, Vancouver, BC V6T 1Z4");
+INSERT INTO Customer VALUES ("mshan", "mshanpw", "shanmarkshan@gmail.com", "6045557782", "2529 West Mall, Vancouver, BC V6T 1Z4");
+INSERT INTO Customer VALUES ("yli", "ylipw", "yvonneli97@gmail.com", "6045554321", "2629 West Mall, Vancouver, BC V6T 1Z4");
+INSERT INTO Customer VALUES ("hughmungus", "hughmunguspw", "humungusyeets@hehe.com", "6045559872","2829 West Mall, Vancouver, BC V6T 1Z4");
+
+INSERT INTO CreditCard VALUES ("4324-0987-1234-8765", "2329 West Mall, Vancouver, BC V6T 1Z4",'2020-02-11', "Raymond Situ", "rsitu");
+INSERT INTO CreditCard VALUES ("1111-9898-7676-5252", "2330 West Mall, Vancouver, BC V6T 1Z4",'2020-08-21', "Kevin Wong", "kwong");
+INSERT INTO CreditCard VALUES ("2222-0000-9999-8888", "2529 West Mall, Vancouver, BC V6T 1Z4",'2021-07-12', "Mark Shan", "mshan");
+INSERT INTO CreditCard VALUES ("4321-0987-7654-1234", "2629 West Mall, Vancouver, BC V6T 1Z4",'2022-06-13', "Yvonne Li", "yli");
+INSERT INTO CreditCard VALUES ("0808-9797-6565-2323", "2829 West Mall, Vancouver, BC V6T 1Z4",'2022-05-14', "Humung Yeets", "hughmungus");
+
+INSERT INTO DeliveryPerson VALUES ("Kristy Kong", "2053 Main Mall, Vancouver, BC V6T 1Z2", 20.21, 3.2, "kristykong@gmail.com", "11114444", "2018-02-11");
+INSERT INTO DeliveryPerson VALUES ("Sharpe Velleman", "2053 West Mall, Vancouver, BC V6T 1Z2", 5.21, 4.2, "sv@gmail.com", "12340987", "2018-03-11");
+INSERT INTO DeliveryPerson VALUES ("Berkowitz Pearson", "2053 East Mall, Vancouver, BC V6T 1Z2", 25.21, 3.1, "bp@gmail.com", "22223333", "2018-04-11");
+INSERT INTO DeliveryPerson VALUES ("Cherry Fizzell", "2053 North Mall, Vancouver, BC V6T 1Z2", 120.21, 3.5, "cf@gmail.com", "33334444", "2018-05-11");
+INSERT INTO DeliveryPerson VALUES ("Veaux Hoshin", "2053 South Mall, Vancouver, BC V6T 1Z2", 202.21, 4.2, "vh@gmail.com", "55554444", "2018-06-11");
+INSERT INTO DeliveryPerson VALUES ("Elon Musk", "2000 Main Mall, Vancouver, BC V6T 1Z2", 20.21, 3.2, "elonmusk@gmail.com", "00009999", "2018-07-11");
+INSERT INTO DeliveryPerson VALUES ("Jeff Bezos", "2022 West Mall, Vancouver, BC V6T 1Z2", 5.21, 4.2, "jeff@gmail.com", "88887777", "2018-08-11");
+INSERT INTO DeliveryPerson VALUES ("Mark Zuckerberg", "2353 East Mall, Vancouver, BC V6T 1Z2", 25.21, 3.1, "mark@gmail.com", "77773333", "2018-02-21");
+INSERT INTO DeliveryPerson VALUES ("Spongebob Squarepants", "2153 Pineapple, Bikini Bottom, BC V6T 1Z2", 120.21, 3.5, "spongebob@gmail.com", "10109292", "2018-02-15");
+INSERT INTO DeliveryPerson VALUES ("Patrick Star", "2053 Rock, Bikini Bottom, BC V6T 1Z2", 202.21, 4.2, "rick@gmail.com", "12341337", "2018-03-15");
+
+INSERT INTO Biker VALUES("Kristy Kong", "2053 Main Mall, Vancouver, BC V6T 1Z2", 10, "cool blue bike");
+INSERT INTO Biker VALUES("Sharpe Velleman", "2053 West Mall, Vancouver, BC V6T 1Z2", 15, "cool red bike");
+INSERT INTO Biker VALUE("Berkowitz Pearson", "2053 East Mall, Vancouver, BC V6T 1Z2", 10, "hot pink bike");
+INSERT INTO Biker VALUES("Cherry Fizzell", "2053 North Mall, Vancouver, BC V6T 1Z2", 5, "coral green bike");
+INSERT INTO Biker VALUES("Veaux Hoshin", "2053 South Mall, Vancouver, BC V6T 1Z2", 10, "watermelon red bike");
+
+INSERT INTO DriverVehicleInformation VALUES ("000000", "Elon123", "2012 Red Tesla Roadster", 1);
+INSERT INTO DriverVehicleInformation VALUES ("111111", "Jeff123", "2099 Rainbow Lamborghini Aventador", 1);
+INSERT INTO DriverVehicleInformation VALUES ("222222", "Mark123", "1962 Green John Deere Tractor", 0);
+INSERT INTO DriverVehicleInformation VALUES ("333333", "Spongebob123", "1999 White Speedboat", 0);
+INSERT INTO DriverVehicleInformation VALUES ("444444", "Patrick123", "2322 Brown Half Sphere Rock", 0);
+
+INSERT INTO Driver VALUES ("Elon Musk", "2000 Main Mall, Vancouver, BC V6T 1Z2", "000000", "Elon123");
+INSERT INTO Driver VALUES ("Jeff Bezos", "2022 West Mall, Vancouver, BC V6T 1Z2", "111111", "Jeff123");
+INSERT INTO Driver VALUES ("Mark Zuckerberg", "2353 East Mall, Vancouver, BC V6T 1Z2", "222222", "Mark123");
+INSERT INTO Driver VALUES ("Spongebob Squarepants", "2153 Pineapple, Bikini Bottom, BC V6T 1Z2", "333333", "Spongebob123");
+INSERT INTO Driver VALUES ("Patrick Star", "2053 Rock, Bikini Bottom, BC V6T 1Z2", "444444", "Patrick123");
+
+INSERT INTO Restaurant VALUES (1, "McDonalds", "101 5728 University Blvd, Vancouver, BC V6T 1K6", "Fast Food", 3.4, 3.25);
+INSERT INTO Restaurant VALUES (2, "Miku", "200 Granville St #70, Vancouver, BC V6C 1S4", "Sushi", 4.5, 4.25);
+INSERT INTO Restaurant VALUES (3, "FreshSlice", "688 Dunsmuir Street Vancouver, British Columbia V6B 1N3", "Pizza", 4.2, 4.00);
+INSERT INTO Restaurant VALUES (4, "Santouka", "558 W Broadway, Vancouver, BC V5Z 1E9", "Ramen", 4.6, 3.00);
+INSERT INTO Restaurant VALUES (5, "Uncle Fatih", "6045 University BLVD Vancouver, BC V6T 1Z1", "Pizza", 3.9, 2.25);
+
+INSERT INTO Menu VALUES (1, "Lunch and Dinner", TIME("11:00"), TIME("6:00"), 1);
+INSERT INTO Menu VALUES (2, "Lunch", TIME("11:00"), TIME("15:00"), 2);
+INSERT INTO Menu VALUES (3, "Happy Hour", TIME("16:00"), TIME("18:00"), 3);
+INSERT INTO Menu VALUES (4, "Dinner", TIME("16:00"), TIME("20:00"), 4);
+INSERT INTO Menu VALUES (5, "All Day", TIME("9:00"), TIME("22:00"), 5);
+
+INSERT INTO MenuCategory VALUES (1,"Burgers",1);
+INSERT INTO MenuCategory VALUES (2,"Appetiziers",2);
+INSERT INTO MenuCategory VALUES (3,"Entree",3);
+INSERT INTO MenuCategory VALUES (4,"Entree",4);
+INSERT INTO MenuCategory VALUES (5,"Entree",5);
+
+INSERT INTO RestaurantOrder VALUES (1, "2019-02-10", TIME("11:10"), 10.00, 4, 1.00, "delivered", "2329 West Mall, Vancouver, BC V6T 1Z4", "rsitu","4324-0987-1234-8765","Kristy Kong", "2053 Main Mall, Vancouver, BC V6T 1Z2", 1);
+INSERT INTO RestaurantOrder VALUES (2, "2019-02-15", TIME("11:12"), 22.25, 14, 1.00, "delivered", "2330 West Mall, Vancouver, BC V6T 1Z4", "kwong","1111-9898-7676-5252","Sharpe Velleman", "2053 West Mall, Vancouver, BC V6T 1Z2", 2);
+INSERT INTO RestaurantOrder VALUES (3, "2019-02-16", TIME("11:15"), 3.00, 12, 1.00, "delivered", "2530 West Mall, Vancouver, BC V6T 1Z4", "mshan","2222-0000-9999-8888","Berkowitz Pearson", "2053 East Mall, Vancouver, BC V6T 1Z2", 3);
+INSERT INTO RestaurantOrder VALUES (4, "2019-02-18", TIME("11:22"), 24.00, 8, 1.00, "delivered", "2630 West Mall, Vancouver, BC V6T 1Z4", "yli","4321-0987-7654-1234","Spongebob Squarepants", "2153 Pineapple, Bikini Bottom, BC V6T 1Z2", 4);
+INSERT INTO RestaurantOrder VALUES (5, "2019-02-24", TIME("11:10"), 5.00, 4, 1.00, "order made", "101 5728 University Blvd, Vancouver, BC V6T 1K6", "hughmungus","0808-9797-6565-2323","Patrick Star", "2053 Rock, Bikini Bottom, BC V6T 1Z2", 5);
+
+INSERT INTO MenuItem VALUES (1, "Big Mac", 1, 5.00, 550, "burger 2 patties", 3.9);
+INSERT INTO MenuItem VALUES (2, "Salmon Sashimi",2, 22.25, 500, "fresh fish", 4.6);
+INSERT INTO MenuItem VALUES (3, "Cheese Pizza Slice", 3, 3.00, 340, "cheese pizza with chedder", 4.2);
+INSERT INTO MenuItem VALUES (4, "Shio Ramen", 4, 12.00, 750, "ramen noodles with pork", 4.5);
+INSERT INTO MenuItem VALUES (5, "Cheese Pizza Slice", 5, 2.50, 370, "a slice of cheese pizza", 3.9);
+
+INSERT INTO OrderedMenuItem VALUES (1, 1, 2, "no pickles");
+INSERT INTO OrderedMenuItem VALUES (2, 2, 1, "");
+INSERT INTO OrderedMenuItem VALUES (3, 3, 1, "");
+INSERT INTO OrderedMenuItem VALUES (4, 4, 2, "");
+INSERT INTO OrderedMenuItem VALUES (5, 5, 1, "");
+
--- server.py
+++ server.py
@@ -13,29 +13,166 @@ engine.execute('USE project')
 def hello():
     return "poggers"
 
+# Returns all customers in the DB
 @app.route("/getCustomers")
 def getCustomers():
     query = 'SELECT * FROM Customer;'
     response = []
     result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No customers found"
     for row in result:
-        customer = dict()
-        customer['CustomerID'] = row[0]
-        customer['EmailAddress'] = row[1]
-        customer['PhoneNumber'] = row[2]
-        customer['Address'] = row[3]
+        customer = dict(row)
         response.append(customer)
     return jsonify(response)
 
-@app.route("/addCustomer", methods=["PUT"])
+# Given a username and password as arguments in a get request, gets a single customer object
+@app.route("/getCustomer")
+def getCustomer():
+    customerUsername = request.args.get('CustomerUsername')
+    customerPassword = request.args.get('CustomerPassword')
+    query = 'SELECT * FROM Customer WHERE CustomerUsername = "{}" AND CustomerPassword = "{}"'.format(customerUsername, customerPassword)
+    response = []
+    try:
+        result = engine.execute(query)
+        if result.rowcount == 0:
+            return "Customer not found"
+        row = result.first()
+        customer = dict(row)
+        response.append(customer)
+        return jsonify(response)
+    except Exception as e:
+        return "Exception thrown"
+
+# Returns all restaurants in the DB
+@app.route("/getRestaurants")
+def getRestaurants():
+    query = 'SELECT * FROM Restaurant'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No restaurants found"
+    for row in result:
+        restaurant = dict(row)
+        response.append(restaurant)
+    return jsonify(response)
+
+# Given a name, gets all restaurants in the DB that have the name in their name
+@app.route("/getRestaurantsFiltered")
+def getRestaurantsFiltered():
+    restaurantName = request.args.get('Name')
+    query = "SELECT * FROM Restaurant WHERE Name LIKE '%%{}%%'".format(restaurantName)
+    response = []
+    try:
+        result = engine.execute(query)
+        if result.rowcount == 0:
+            return "No restaurants found"
+        for row in result:
+            restaurant = dict(row)
+            response.append(restaurant)
+        return jsonify(response)
+    except Exception as e:
+        return "Exception thrown"
+
+# Given the restaurant ID, get a single restaurant (wrapped in an array)
+@app.route("/getRestaurant")
+def getRestaurant():
+    try:
+        restaurantID = request.args.get('RestaurantID')
+        query = 'SELECT * FROM Restaurant WHERE RestaurantID = {}'.format(restaurantID)
+        response = []
+        result = engine.execute(query)
+        if result.rowcount == 0:
+            return "Restaurant not found"
+        row = result.first()
+        restaurant = dict(row)
+        response.append(restaurant)
+        return jsonify(response)
+    except Exception as e:
+        return "Exception thrown"
+
+# Get all restaurant orders in the DB
+@app.route("/getRestaurantOrders")
+def getRestaurantOrders():
+    query = 'SELECT * FROM RestaurantOrder'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No restaurant orders found"
+    for row in result:
+        restaurantOrder = dict(row)
+        restaurantOrder['Time'] = str(restaurantOrder['Time'])
+        response.append(restaurantOrder)
+    return jsonify(response)
+
+# Get all restaurant menus in  the DB
+@app.route("/getMenus")
+def getMenus():
+    query = 'SELECT * FROM Menu'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No menus found"
+    for row in result:
+        menu = dict(row)
+        menu['TimeFrom'] = str(menu['TimeFrom'])
+        menu['TimeTo'] = str(menu['TimeTo'])
+        response.append(menu)
+    return jsonify(response)
+
+# Get all menu categories in the DB
+@app.route("/getMenuCategories")
+def getMenuCategories():
+    query = 'SELECT * FROM MenuCategory'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No menu categories found"
+    for row in result:
+        menuCategory = dict(row)
+        response.append(menuCategory)
+    return jsonify(response)
+
+# Get all menu items in the DB
+@app.route("/getMenuItems")
+def getMenuItems():
+    query = 'SELECT * FROM MenuItem'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No menu items found"
+    for row in result:
+        menuItem = dict(row)
+        response.append(menuItem)
+    return jsonify(response)
+
+#Get all ordered menu items from the DB
+@app.route("/getOrderedMenuItems")
+def getMOrderedenuItems():
+    query = 'SELECT * FROM OrderedMenuItem'
+    response = []
+    result = engine.execute(query)
+    if result.rowcount == 0:
+        return "No ordered menu items found"
+    for row in result:
+        orderedMenuItem = dict(row)
+        response.append(orderedMenuItem)
+    return jsonify(response)
+
+# Given all the fields in a POST form, create a new customer
+@app.route("/addCustomer", methods=["POST"])
 def addCustomer():
-    customerID = request.form['CustomerID']
+    customerUsername = request.form['CustomerUsername']
+    customerPassword = request.form['CustomerPassword']
     emailAddress = request.form['EmailAddress']
     phoneNumber = request.form['PhoneNumber']
     address = request.form['Address']
-    query = 'INSERT INTO Customer VALUES ({}, "{}", "{}", "{}");'.format(customerID, emailAddress, phoneNumber, address)
-    result = engine.execute(query)
-    return 'successfully added {} {} {} {}'.format(customerID, emailAddress, phoneNumber, address)
+    query = 'INSERT INTO Customer VALUES ("{}", "{}", "{}", "{}", "{}");'.format(customerUsername, customerPassword, emailAddress, phoneNumber, address)
+    try:
+        result = engine.execute(query)
+    except Exception as e:
+        return "Error in backend database"
+    return 'Successfully added {} {} {} {} {}'.format(customerUsername, customerPassword, emailAddress, phoneNumber, address)
 
 if __name__ == '__main__':
     app.run(debug=True)
