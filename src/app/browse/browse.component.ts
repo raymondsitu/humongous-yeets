5dc0b5b0b57216b064d38fe7106a064f14abde2c
Kevin Wong <kev.wong97@gmail.com>
2019-03-17 13:26:48 -0700

    added browse restaurants
    populated browse tab with a table of all restaurants and a simple search bar that filters out restaurants based on name

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../http.service';
 
 @Component({
   selector: 'app-browse',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
+  restaurantName: string;
+  restaurants: any[];
+  displayedColumns = ['Name', 'Location', 'Category', 'Rating', 'DeliveryFee'];
 
-  constructor() { }
+  constructor(private http: HttpService) {
+    http.getRequest('/getRestaurants', {}).then((restaurants) => {
+      this.restaurants = restaurants;
+    })
+  }
+
+  searchRestaurants(): void {
+    this.http.getRequest('/getRestaurantsFiltered', {Name: this.restaurantName}).then((restaurants) => {
+      this.restaurants = restaurants;
+    }).catch((response) => {
+      alert('No restaurants found');
+    })
+  }
 
   ngOnInit() {
   }
